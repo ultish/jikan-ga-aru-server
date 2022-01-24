4adb636bc24255c4007d050a51dcffc27f0eafa3
Jimmy <ultish@gmail.com>
2022-01-24 18:07:41 +1100

    remove timesheet type

--- src/main/kotlin/com/ultish/jikangaaruserver/entities/ETimesheet.kt
+++ src/main/kotlin/com/ultish/jikangaaruserver/entities/ETimesheet.kt
@@ -1,31 +1,23 @@
 package com.ultish.jikangaaruserver.entities
 
-import com.querydsl.core.annotations.QueryEntity
-import com.ultish.generated.types.Timesheet
-import org.bson.types.ObjectId
-import org.springframework.data.annotation.Id
-import org.springframework.data.mongodb.core.index.Indexed
-import org.springframework.data.mongodb.core.mapping.Document
-import java.util.*
-
-@Document(value = "timesheet")
-@QueryEntity
-data class ETimesheet(
-   @Id
-   val id: String = ObjectId().toString(),
-   val weekEndingDate: Date,
-   @Indexed
-   val userId: String,
-   @Indexed
-   val timeChargeTotalIds: List<String>,
-   @Indexed
-   val trackedDayIds: List<String>,
-) : GraphQLEntity<Timesheet> {
-   override fun toGqlType(): Timesheet =
-      Timesheet(
-         id,
-         weekEndingDate.time.toDouble()
-      )
-
-   override fun id(): String = id
-}
+//@Document(value = "timesheet")
+//@QueryEntity
+//data class ETimesheet(
+//   @Id
+//   val id: String = ObjectId().toString(),
+//   val weekEndingDate: Date,
+//   @Indexed
+//   val userId: String,
+//   @Indexed
+//   val timeChargeTotalIds: List<String>,
+//   @Indexed
+//   val trackedDayIds: List<String>,
+//) : GraphQLEntity<Timesheet> {
+//   override fun toGqlType(): Timesheet =
+//      Timesheet(
+//         id,
+//         weekEndingDate.time.toDouble()
+//      )
+//
+//   override fun id(): String = id
+//}
--- src/main/kotlin/com/ultish/jikangaaruserver/timesheets/TimesheetRepository.kt
+++ src/main/kotlin/com/ultish/jikangaaruserver/timesheets/TimesheetRepository.kt
@@ -1,9 +1,9 @@
 package com.ultish.jikangaaruserver.timesheets
 
-import com.ultish.jikangaaruserver.entities.ETimesheet
-import org.springframework.data.mongodb.repository.MongoRepository
-import org.springframework.data.querydsl.QuerydslPredicateExecutor
-
-interface TimesheetRepository : MongoRepository<ETimesheet, String>,
-   QuerydslPredicateExecutor<ETimesheet> {
-}
+//import com.ultish.jikangaaruserver.entities.ETimesheet
+//import org.springframework.data.mongodb.repository.MongoRepository
+//import org.springframework.data.querydsl.QuerydslPredicateExecutor
+//
+//interface TimesheetRepository : MongoRepository<ETimesheet, String>,
+//   QuerydslPredicateExecutor<ETimesheet> {
+//}
